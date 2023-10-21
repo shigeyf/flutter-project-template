69b846cbb1e3f3e1965ac65c5fcfc093a354f161
Shige Fukushima <shigeyf@live.com>
2023-10-22 03:59:56 +0900

    feat: 🎸 Added localization (L10N) feature

--- apps/simple_riverpod_arch/lib/src/app.dart
+++ apps/simple_riverpod_arch/lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import './app_themes.dart';
 import '../src/presentation/views/home_page.dart';
 
@@ -11,6 +12,10 @@ class App extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       // App ThemeData
       theme: themeData,
+      // Localization (L10N)
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      // Supported languages for Localization (L10N)
+      supportedLocales: AppLocalizations.supportedLocales,
       // Home
       home: const HomePage(),
     );
--- apps/simple_riverpod_arch/lib/src/presentation/views/home_page.dart
+++ apps/simple_riverpod_arch/lib/src/presentation/views/home_page.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 import '../../application/entry_service.dart';
 import '../controllers/home_page_controller.dart';
+import '../../localization/app_localizations_context.dart';
 
 class HomePage extends ConsumerWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  onAdd(WidgetRef ref) {
-    ref.read(homePageControllerProvider).addEntry(DateTime.now().toString());
+  onAdd(BuildContext context, WidgetRef ref) {
+    ref
+        .read(homePageControllerProvider)
+        .addEntry(DateFormat(context.locale.dateFormat).format(DateTime.now()));
   }
 
   onRemove(WidgetRef ref, String entry) {
@@ -18,7 +22,7 @@ class HomePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Entries Management'),
+        title: Text(context.locale.appBarTitle),
       ),
       body: ref.watch(entriesProvider).when(
             loading: () => const Center(child: CircularProgressIndicator()),
@@ -36,7 +40,7 @@ class HomePage extends ConsumerWidget {
           ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () => onAdd(ref),
+        onPressed: () => onAdd(context, ref),
       ),
     );
   }
