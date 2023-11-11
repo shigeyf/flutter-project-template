4ee87d4f842d5f4f6726a4943432adb45612e2d5
Shige Fukushima <shigeyf@live.com>
2023-11-12 01:30:08 +0900

    refactor: 💡 Updated

--- apps/riverpod_todo/lib/src/features/todo/data/repositories/todo_list_repository.dart
+++ apps/riverpod_todo/lib/src/features/todo/data/repositories/todo_list_repository.dart
@@ -11,23 +11,6 @@ class TodoList extends Notifier<List<Todo>> {
         const Todo(id: 'todo-0', description: 'Buy cookies'),
         const Todo(id: 'todo-1', description: 'Star Riverpod'),
         const Todo(id: 'todo-2', description: 'Have a walk'),
-        const Todo(id: 'todo-3', description: 'Buy cookies'),
-        const Todo(id: 'todo-4', description: 'Star Riverpod'),
-        const Todo(id: 'todo-5', description: 'Have a walk'),
-        const Todo(id: 'todo-6', description: 'Buy cookies'),
-        const Todo(id: 'todo-7', description: 'Star Riverpod'),
-        const Todo(id: 'todo-8', description: 'Have a walk'),
-        const Todo(id: 'todo-9', description: 'Buy cookies'),
-        const Todo(id: 'todo-10', description: 'Star Riverpod'),
-        const Todo(id: 'todo-11', description: 'Have a walk'),
-        const Todo(id: 'todo-12', description: 'Buy cookies'),
-        const Todo(id: 'todo-13', description: 'Star Riverpod'),
-        const Todo(id: 'todo-14', description: 'Have a walk'),
-        const Todo(id: 'todo-15', description: 'Buy cookies'),
-        const Todo(id: 'todo-16', description: 'Star Riverpod'),
-        const Todo(id: 'todo-17', description: 'Have a walk'),
-        const Todo(id: 'todo-18', description: 'Buy cookies'),
-        const Todo(id: 'todo-19', description: 'Star Riverpod'),
       ];
 
   void add(String description) {
